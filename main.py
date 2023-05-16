1cc9feb9ef222273b590e050f8c67f2daafeaa31
willian pires <willian_pires@hotmail.com>
2023-05-16 16:42:15 -0500

    WIP real multiprocess

--- main.py
+++ main.py
@@ -1,88 +1,101 @@
 
-import pymysql.cursors
-import json
-import requests
-from datetime import datetime
-from threading import Thread
-import concurrent.futures
 
+from datetime import datetime
+from time import sleep
+
+import pymysql.cursors
+import multiprocessing as mpc
+import requests
 import os
 
-API_HTTP_HOST=os.environt.get('HOST','http://127.0.0.1:3001')
 
-DBHOST=os.environt.get('DBHOST','localhost')
-DBUSER=os.environt.get('DBUSER','dbuser')
-DBPASS=os.environt.get('DBPASS','dbpass')
-DBNAME=os.environt.get('DBNAME','dbname')
+API_HTTP_HOST=os.getenv('API_HTTP_HOST','http://127.0.0.1:3001')
+
+DBHOST=os.getenv('DBHOST','localhost')
+DBUSER=os.getenv('DBUSER','dbuser')
+DBPASS=os.getenv('DBPASS','dbpass')
+DBNAME=os.getenv('DBNAME','dbname')
+TPROCS=int(os.getenv('TPROCS', 8))
 
 
+def senddata(data):
+    S = requests.Session()
+    _url = f'{API_HTTP_HOST}/v1/virt_control/vmData'
+    _p = S.post(_url, json=data)
+    if _p.ok:
+        return True
+    else:
+        return False
 
+def senddatamass(data):
+    S = requests.Session()
+    _url = f'{API_HTTP_HOST}/v1/virt_control/vmMassData'
+    _p = S.post(_url, json={'data': data})
+
+    if _p.ok:
+        return True
+    else:
+        return False
+
+def generatedaterange():
+    date_ini = int(datetime.timestamp(datetime.strptime('2023-02-15 00:00:00', "%Y-%m-%d %H:%M:%S")))
+    date_acctual = date_ini
+    total_sum = 300
+    days = 90
+    UNIXDAY = 86400
+    total_ranges = int((UNIXDAY * days)/total_sum)
+    ret = []
+    for _ in range(total_ranges):
+        ret.append((int(date_acctual), int(date_acctual+total_sum)))
+        date_acctual += total_sum + 1
+    return ret
 
 def main():
-    def sendData(data):
-        S = requests.Session()
-        _url = f'{API_HTTP_HOST}/v1/virt_control/vmData'
-        _p = S.post(_url, json=data)
-        if _p.ok:
-            return True
-        else:
-            return False
-
-    def sendDataMass(data):
-        S = requests.Session()
-        _url = f'{API_HTTP_HOST}/v1/virt_control/vmMassData'
-        _p = S.post(_url, json=data)
-        if _p.ok:
-            return True
-        else:
-            return False
-
 
     connection = pymysql.connect(host=DBHOST,
                                 user=DBUSER,
                                 password=DBPASS,
                                 database=DBNAME,
                                 cursorclass=pymysql.cursors.DictCursor)
-    dateRange = generateDateRange()
-    totalRange = len(dateRange)
-    string_print = "%04d/%04d -- Executando data: %s Total de registros: %d"
+    
+    daterange = generatedaterange()
+    totalrange = len(daterange)
+    string_print = "%04d/%04d -- Executando data: %s Total de registros: %d -- Processos ativos %02d"
     seq = 0
 
-    MASS = False
-    for date_ini,date_end in dateRange:
+    for date_ini,date_end in daterange:
+
+        t_actives = len(mpc.active_children())
+        mdata = []
+
+        while True:
+            if len(mpc.active_children()) < TPROCS:
+                print("")
+                break
+            print(".",end="")
+            sleep(5)
+
+
         with connection.cursor() as cursor:
+            
             sql = "select * from virt_manager_data  where clock >= %s and clock <= %s and host is not null;"
             cursor.execute(sql, (date_ini,date_end) )
             results = cursor.fetchall()
-            totalResults = len(results)
-            if totalResults == 0:
+            totalresults = len(results)
+            if totalresults == 0:
                 continue
 
-            pool = concurrent.futures.ThreadPoolExecutor(max_workers=16)
-            mdata = []
-            print(string_print % (seq, totalRange, datetime.fromtimestamp(date_end), totalResults))
+            
+            print(string_print % (seq, totalrange, datetime.fromtimestamp(date_end), totalresults, t_actives), end="")
             for result in results:
-                if not MASS:
-                    pool.submit(sendData(result))
-                if MASS:
-                    mdata.append(result)
-            if MASS:
-                pool.submit(sendDataMass({'data': mdata}))
-            pool.shutdown(wait=True)
+                mdata.append(result)
+                
+            t = mpc.Process(target=senddatamass, args=([mdata]))
+            t.start()
+
         seq += 1
 
-def generateDateRange():
-    date_ini = int(datetime.timestamp(datetime.strptime('2023-02-10 00:00:00', "%Y-%m-%d %H:%M:%S")))
-    date_acctual = date_ini
-    sum = 3600
-    days = 90
-    UNIXDAY = 86400
-    total_ranges = int((UNIXDAY * days)/sum)
-    ret = []
-    for _ in range(total_ranges):
-        ret.append((int(date_acctual), int(date_acctual+sum)))
-        date_acctual += sum + 1
-    return ret
+
     
 try:
     main()
